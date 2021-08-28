d9125d9f37396a05924b12e5341c9398b6c02408
Bharat Rathi <rathibharat27412@gmail.com>
2021-08-28 12:30:22 +0530

    Revert "Frontend login signUp"
    This reverts commit 558fa296edd351aa592064e2cc303643c8383e74.

--- frontend/learnit/src/App.js
+++ frontend/learnit/src/App.js
@@ -1,19 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
-import SignIn from "./SignIn"
-import SignUp from './SignUp';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/sign-up' component={SignUp} />
-          <Route path='/sign-in' component={SignIn} />
-        </Switch>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
